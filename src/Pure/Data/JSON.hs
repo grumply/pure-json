dded00941f5bb19188bfdd64c0234f09637e7f87
grumply <sean@grump.ly>
2018-12-30 02:46:57 -0600

    Unify encode type across GHC and GHCJS.

--- src/Pure/Data/JSON.hs
+++ src/Pure/Data/JSON.hs
@@ -1,10 +1,21 @@
 {-# LANGUAGE CPP #-}
-module Pure.Data.JSON (parse,module Export) where
+module Pure.Data.JSON (parse,decode,module Export) where
+
+import Pure.Data.Txt (fromTxt,Txt)
 
 #ifdef __GHCJS__
-import Pure.Data.JSON.GHCJS as Export
+import           Pure.Data.JSON.GHCJS as Export hiding (decode)
+import qualified Pure.Data.JSON.GHCJS as GHCJS
 #else
-import Pure.Data.JSON.GHC   as Export
+import           Pure.Data.JSON.GHC   as Export hiding (decode)
+import qualified Pure.Data.JSON.GHC   as GHC
 #endif
 
 parse = flip parseMaybe
+
+decode :: FromJSON a => Txt -> Maybe a
+#ifdef __GHCJS__
+decode = GHCJS.decode
+#else
+decode = GHC.decode . fromTxt
+#endif
