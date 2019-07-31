a53a546e1a10a908c9ac6af4fcb15959a9f6c463
Sean Mackenzie <sean@grumply.com>
2019-07-31 15:31:54 -0500

    Refine encode/decode interface consistency across ghc/ghcjs.

--- default.nix
+++ default.nix
@@ -1,12 +1,12 @@
 { mkDerivation, aeson, aeson-pretty, base, pure-txt, stdenv, text
-, unordered-containers, vector
+, unordered-containers, vector, bytestring
 }:
 mkDerivation {
   pname = "pure-json";
   version = "0.7.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    aeson aeson-pretty base pure-txt text unordered-containers vector
+    aeson aeson-pretty base pure-txt text unordered-containers vector bytestring
   ];
   homepage = "github.com/grumply/pure-json";
   license = stdenv.lib.licenses.bsd3;
--- src/Pure/Data/JSON.hs
+++ src/Pure/Data/JSON.hs
@@ -1,21 +1,50 @@
 {-# LANGUAGE CPP #-}
-module Pure.Data.JSON (parse,decode,module Export) where
+module Pure.Data.JSON (parse,decode,encode,decodeBS,encodeBS,module Export) where
 
-import Pure.Data.Txt (fromTxt,Txt)
+import Pure.Data.Txt (Txt,ToTxt(..),FromTxt(..))
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
 
 #ifdef __GHCJS__
-import           Pure.Data.JSON.GHCJS as Export hiding (decode)
+import           Pure.Data.JSON.GHCJS as Export hiding (encode,decode)
 import qualified Pure.Data.JSON.GHCJS as GHCJS
 #else
-import           Pure.Data.JSON.GHC   as Export hiding (decode)
+import           Pure.Data.JSON.GHC   as Export hiding (encode,decode)
 import qualified Pure.Data.JSON.GHC   as GHC
 #endif
 
+{-# INLINE parse #-}
 parse = flip parseMaybe
 
+{-# INLINE decode #-}
 decode :: FromJSON a => Txt -> Maybe a
 #ifdef __GHCJS__
 decode = GHCJS.decode
 #else
 decode = GHC.decode . fromTxt
+#endif
+
+{-# INLINE encode #-}
+encode :: ToJSON a => a -> Txt
+#ifdef __GHCJS__
+encode = GHCJS.encode . toJSON
+#else
+encode = toTxt . GHC.encode
+#endif
+
+{-# INLINE decodeBS #-}
+decodeBS :: FromJSON a => BSL.ByteString -> Maybe a
+#ifdef __GHCJS__
+decodeBS = GHCJS.decode . toTxt
+#else
+decodeBS = GHC.decode
+#endif
+
+{-# INLINE encodeBS #-}
+encodeBS :: ToJSON a => a -> BSL.ByteString
+#ifdef __GHCJS__
+encodeBS = fromTxt . GHCJS.encode . toJSON
+#else
+encodeBS = GHC.encode
 #endif
